0d5526afa98f1615a3f536051689a6f45ebe6df6
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-07 14:02:30 +0300

    Init project and base models

---
+++ OnlineCinema/OC.Domain/Intefaces/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+namespace OC.Domain.Intefaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> InsertAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> RemoveAsync(T entity);
+        Task<bool> IsExistAsync(Expression<Func<T, bool>> match, bool isTracking = false);
+        Task<T> FindByIdAsync(object id);
+        Task<List<T>> GetAllAsync(bool isTracking = false);
+        Task<List<T>> GetAllAsync(int offset, int count, bool isTracking = false);
+        Task<long> CountAsync(bool isTracking = false);
+        Task<long> CountAsync(Expression<Func<T, bool>> match, bool isTracking = false);
+        Task<List<T>> GetListAsync(Expression<Func<T, bool>> match, bool isTracking = false);
+        Task<T> GetFirstAsync(Expression<Func<T, bool>> match, bool isTracking = false);
+        void Dispose();
+    }
+}
---
+++ OnlineCinema/OC.Domain/Models/BaseModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OC.Domain.Models
+{
+    public class BaseCreateModel
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public string CreatedFromIP { get; set; }
+    }
+
+    public class BaseEditModel
+    {
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+        public string UpdatedFromIP { get; set; }
+    }
+
+    public class BaseModel : BaseEditModel
+    {
+
+    }
+
+    public class BaseModel<TypeId> : BaseModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public TypeId Id { get; set; }
+    }
+
+    public class BaseModelWithoutGeneratingId<TypeId> : BaseModel
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public TypeId Id { get; set; }
+    }
+}
