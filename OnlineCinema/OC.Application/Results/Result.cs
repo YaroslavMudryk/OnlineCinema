014633fae2c817ef980d6bed40723a6a39ab84d1
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-07 15:02:40 +0300

    Add user services

---
+++ OnlineCinema/OC.Application/Results/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OC.Application.Results
+{
+    public class Result
+    {
+        public Result(string error) : this(false, error, null)
+        {
+
+        }
+        public Result(object data) : this(true, null, data)
+        {
+
+        }
+
+        public Result(bool isOK, string error, object data)
+        {
+            if (data == null)
+            {
+                IsOK = false;
+                if (string.IsNullOrEmpty(error))
+                    Error = "Resource not found";
+                else
+                    Error = error;
+                Data = null;
+            }
+        }
+        public bool IsOK { get; set; } = true;
+        public string Error { get; set; } = null;
+        public object Data { get; set; } = 0;
+    }
+}
--- OnlineCinema/OC.Infrastructure.IoC/DependencyContainer.cs
+++ OnlineCinema/OC.Infrastructure.IoC/DependencyContainer.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using OC.Application.Services.Implementations;
+using OC.Application.Services.Intefaces;
 using OC.Domain.Intefaces;
 using OC.Infrastructure.Data.Repositories.EF;
 using System;
@@ -10,6 +12,7 @@ namespace OC.Infrastructure.IoC
         public static void AddCinemaServices(this IServiceCollection services)
         {
             services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserService, UserService>();
         }
     }
 }
