3e5aa2e5952bd785e6acfddc53c785300a8a35c0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-07 14:07:39 +0300

    Add IUserRepository

---
+++ OnlineCinema/OC.Domain/Intefaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using OC.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OC.Domain.Intefaces
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        Task<List<User>> SearchUsersAsync(string name, int afterId);
+    }
+}
