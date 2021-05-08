d14edd56bc7dcb7ace5a648f4502274fe06b860d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-08 20:15:04 +0300

    Delete migrations and added new fileds to UserCreateModel

--- OnlineCinema/OC.Application/ViewModels/Users/UserCreateModel.cs
+++ OnlineCinema/OC.Application/ViewModels/Users/UserCreateModel.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace OC.Application.ViewModels.Users
 {
     public class UserCreateModel
     {
+        [Required, MinLength(5), MaxLength(200)]
+        public string Login { get; set; }
+        [Required]
+        public string Password { get; set; }
         [Required, MinLength(2), MaxLength(150)]
         public string Name { get; set; }
         [MinLength(3), MaxLength(30)]
