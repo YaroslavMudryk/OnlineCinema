508efbb1b6f6231974d9d2fee95a381797a8af6d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-08 20:33:13 +0300

    Added repositories and ctor in AuthenticationService

--- OnlineCinema/OC.Application/Services/Implementations/AuthenticationService.cs
+++ OnlineCinema/OC.Application/Services/Implementations/AuthenticationService.cs
@@ -1,11 +1,33 @@
 ﻿using OC.Application.Services.Intefaces;
 using OC.Application.ViewModels.Authentications;
+using OC.Domain.Intefaces;
 using System;
 using System.Threading.Tasks;
 namespace OC.Application.Services.Implementations
 {
     public class AuthenticationService : IAuthenticationService
     {
+        private readonly IRoleRepository _roleRepository;
+        private readonly IUserLoginRepository _userLoginRepository;
+        private readonly IUserRoleRepository _userRoleRepository;
+        private readonly IUserSessionRepository _userSessionRepository;
+        private readonly IUserRepository _userRepository;
+
+        public AuthenticationService(
+            IRoleRepository roleRepository,
+            IUserLoginRepository userLoginRepository,
+            IUserRoleRepository userRoleRepository,
+            IUserSessionRepository userSessionRepository,
+            IUserRepository userRepository)
+        {
+            _roleRepository = roleRepository;
+            _userLoginRepository = userLoginRepository;
+            _userRoleRepository = userRoleRepository;
+            _userSessionRepository = userSessionRepository;
+            _userRepository = userRepository;
+        }
+
+
         public async Task<ChangePasswordViewModel> ChangePasswordAsync(ChangePasswordModel changePasswordModel)
         {
             throw new NotImplementedException();
