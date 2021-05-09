f880c4d2a5ec60be34e60944f5923355489704b9
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-05-09 13:26:28 +0300

    Added LoginResult

---
+++ OnlineCinema/OC.Application/Results/Auth/LoginResult.cs
@@ -0,0 +1,8 @@
+﻿using OC.Application.ViewModels.Authentications;
+namespace OC.Application.Results.Auth
+{
+    public class LoginResult : Result<LoginViewModel>
+    {
+        public LoginResult(LoginViewModel model) : base(model) { }
+    }
+}
