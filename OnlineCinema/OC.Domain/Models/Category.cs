249026a0e3a3afa0925f81246700400884af1154
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-06-06 02:07:46 +0300

    Added Category model and inteface repository

---
+++ OnlineCinema/OC.Domain/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace OC.Domain.Models
+{
+    public class Category : BaseModel<int>
+    {
+        [Required, MinLength(2), MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
