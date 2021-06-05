d3a2c85e7ed1977a0afd47c2dd8aa11ffe5373a0
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-06-06 02:17:05 +0300

    Added Movie model and repository

---
+++ OnlineCinema/OC.Domain/Models/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OC.Domain.Models
+{
+    public class Movie : BaseModel<long>
+    {
+        [Required, MinLength(2), MaxLength(100)]
+        public string Name { get; set; }
+        [MinLength(5), MaxLength(1000)]
+        public string Description { get; set; }
+        [Required]
+        public DateTime PremiereAt { get; set; }
+        public string AvatarFull { get; set; }
+        public string AvatarSmall { get; set; }
+        public string Cover { get; set; }
+        [Required]
+        public bool IsAdult { get; set; }
+        public string TrailerUrl { get; set; }
+        public string FilmUrl { get; set; }
+    }
+}
